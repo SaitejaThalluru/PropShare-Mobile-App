eb233f5244befdf1a597c185613781be659fbe00
Thalluru Saiteja <thalluru.s@mantralabsglobal.com>
2023-06-21 18:15:49 +0530

    Sample Test Run Test Suite

---
+++ Test Suites/Test Run Sample.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test Run Sample</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>a607a1d6-814b-4e9a-bb02-81bbefde010b</testSuiteGuid>
+   <testCaseLink>
+      <guid>cbe6a586-444b-42ed-a92f-4a6061842e60</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Sample Test Run</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
